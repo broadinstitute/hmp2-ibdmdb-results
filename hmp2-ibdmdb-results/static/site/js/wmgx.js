74a1763b6962a4cf7ddf649feb996ccc9e009c0e
Cesar Arze <cesar.arze@gmail.com>
2018-03-15 17:34:23 -0400

    Adding MVX and 16S biopsy viz javascript and re-organizing MGX js.

---
+++ hmp2-ibdmdb-results/static/site/js/wmgx.js
@@ -0,0 +1,23 @@
+/**
+ * Javascript needed for dataset summary metagenomic functionality on the
+ * IBDMDB website.
+ */
+
+ jQuery(document).ready(function(){
+    var func_pathways_count_table = $('#func_pathways_count_table').DataTable({
+        ajax: {
+            url: 'data/all_average_pathways_names.json',
+            dataSrc: ''
+        },
+        pageLength: 15,
+        searching: false,
+        deferLoading: 0,
+        lengthChange: false,
+        scrollCollapse: false,
+        columns: [
+            {data: 'Pathway'},
+            {data: 'Average abundance'},
+            {data: 'Variance'}
+        ]
+    });
+ })
